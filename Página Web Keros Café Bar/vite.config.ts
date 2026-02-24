1fc443d5b3357ea5af84384bf9eac9c853add6ca
awua-yt <awua171717@gmail.com>
2026-02-24 16:52:40 +0000

    arreglo definitivo

--- Página Web Keros Café Bar/vite.config.ts
+++ Página Web Keros Café Bar/vite.config.ts
@@ -1,15 +1,10 @@
   import { defineConfig } from 'vite';
   import react from '@vitejs/plugin-react-swc';
   import path from 'path';
-  import Sitemap from 'vite-plugin-sitemap'
 
   export default defineConfig({
     plugins: [
-      react(),
-      Sitemap({ 
-        hostname: 'https://keros-cafe.vercel.app/',
-        generateRobotsTxt: true 
-      })
+      react()
     ],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
